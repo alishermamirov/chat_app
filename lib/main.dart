e1aec38e849b621d689fcf08a35ef8ff8c30b2ad
Alisher Mamirov <alishermamirov452@gmail.com>
2024-08-14 20:50:49 +0500

    chat added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,38 @@
-import 'package:chat_app/screens/auth_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'screens/auth_screen.dart';
 import 'screens/chat_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const AuthScreen(),
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, AsyncSnapshot<User?> userSnapshot) {
+          if (userSnapshot.hasData) {
+            return const ChatScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,32 +1,95 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+import '../widgets/messages.dart';
+import '../widgets/new_message.dart';
 
-  static const routeName = "/chat";
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    requestNotificationPermission();
+    super.initState();
+  }
+
+  void requestNotificationPermission() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    fcm.subscribeToTopic('flutterchat');
+
+    // print('User granted permission: ${settings.authorizationStatus}');
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      // print('Got a message whilst in the foreground!');
+      // print('Message data: ${message.data}');
+
+      // if (message.notification != null) {
+      // print('Message also contained a notification: ${message.notification}');
+      // }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      // print('Got a message whilst in the background!');
+      // print('Message data: ${message.data}');
+      // if (message.notification != null) {
+      // print('Message also contained a notification: ${message.notification}');
+      // }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text("salom"),
-        ),
+      appBar: AppBar(
+        title: const Text('Flutter Chat'),
+        actions: [
+          DropdownButton(
+            underline: Container(),
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            items: const [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Row(
+                  children: [
+                    Icon(Icons.exit_to_app),
+                    SizedBox(width: 8),
+                    Text('Chiqish'),
+                  ],
+                ),
+              )
+            ],
+            onChanged: (selectedItem) {
+              if (selectedItem == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection("chats/BEspLY2dNQKYILNtwwX9/messages")
-              .snapshots()
-              .listen((data) {
-            print(data.docs);
-          });
-        },
-        child: Icon(Icons.add),
+      body: const Column(
+        children: [
+          Expanded(
+            child: Messages(),
+          ),
+          NewMessages()
+        ],
       ),
     );
   }
